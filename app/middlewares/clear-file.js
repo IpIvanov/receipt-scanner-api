6ed20af20d16e396c6ff281f89c6484511457a9b
Simeon Minchev <Simeon.Minchev@fourth.com>
2017-07-19 17:26:16 +0300

    Add tesseract.js

--- app/middlewares/clear-file.js
+++ app/middlewares/clear-file.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 module.exports = function (req, res, next) {
-  // fs.unlink(req.file.path, function(error) {
-  //   if (error) {
-  //     res.status(500).end();
-  //   }
+  fs.unlink(req.file.path, function(error) {
+    if (error) {
+      res.status(500).end();
+    }
     res.end();
-  // })
+  });
 }
